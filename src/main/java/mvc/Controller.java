5d47836cfa96326a845347c3424f50bb7827ab8d
Óscar Belmonte Fernández <oscar.belmonte@uji.es>
2015-09-29 15:11:25 +0200

    Radio buttons instead of Combo box to select the room.

--- src/main/java/mvc/Controller.java
+++ src/main/java/mvc/Controller.java
@@ -1,27 +1,27 @@
 package mvc;
 
 import data.MetaData;
-import data.Reading;
 import data.Readings;
 import data.Room;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.Slider;
 
 import java.net.URL;
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
-import java.util.Set;
 
 public class Controller implements Initializable, ChangeListener<Number> {
     @FXML
@@ -29,36 +29,65 @@ public class Controller implements Initializable, ChangeListener<Number> {
     @FXML
     private Label numberWAPs;
     @FXML
-    private ComboBox<String> roomCombo;
-    @FXML
     private BarChart<String, Number> readingsChart;
     @FXML
     private CategoryAxis waps;
     private Readings readings;
     private MetaData metaData;
-    private Set<Room> roomSet = new HashSet<>();
+
+    @FXML
+    private RadioButton kitchen;
+    @FXML
+    private RadioButton diningroom;
+    @FXML
+    private RadioButton balcony;
+    @FXML
+    private RadioButton bedroom1;
+    @FXML
+    private RadioButton bedroom2;
+    @FXML
+    private RadioButton bedroom3;
+    @FXML
+    private RadioButton corridor;
+    @FXML
+    private RadioButton drawingroom;
+    @FXML
+    private RadioButton wc1;
+    @FXML
+    private RadioButton wc2;
+
+    private Map<String, XYChart.Series> seriesMap = new HashMap<>();
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         mySlider.valueProperty().addListener((a, b, c) -> {
             numberWAPs.setText(readings.getReadingsGreeterOrEqualTo(c.intValue()).getNumberOfReadings() + "");
         });
-
-        populateRoomCombo();
-
-        roomCombo.valueProperty().addListener((observable, oldValue, newValue) -> {
-            System.out.println(newValue);
-            updateChart(Room.valueOf(newValue));
-        });
     }
 
     private void updateChart(Room room) {
-        if(roomSet.contains(room) == false) {
+        if(seriesMap.containsKey(room.toString()) == false) {
             Readings readingsRoom = readings.getVisibleReadingByRoom(room);
-            if(roomSet.isEmpty()) setSeries(readingsRoom, room.toString());
+            if(seriesMap.containsKey("ALL")) setSeries(readingsRoom, room.toString());
             else addSeries(readingsRoom, room.toString());
-            roomSet.add(room);
         }
+    }
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        RadioButton source = (RadioButton)(event.getSource());
+        String room = source.getId().toUpperCase();
+        if(source.isSelected() == true) {
+            updateChart(Room.valueOf(room));
+        } else if(source.isSelected() == false){
+            removeSeries(Room.valueOf(room));
+        }
+    }
+
+    private void removeSeries(Room room) {
+        readingsChart.getData().removeAll(seriesMap.get(room.toString()));
+        seriesMap.remove(room.toString());
     }
 
     private XYChart.Series<String, Number> newSeries(Readings readingsRoom, String label) {
@@ -71,17 +100,22 @@ public class Controller implements Initializable, ChangeListener<Number> {
             categoryName = metaData.getWAPByIndex(i).getName();
             serie.getData().add(new XYChart.Data<>(categoryName, categoryData));
         }
+        seriesMap.put(label, serie);
         return serie;
     }
 
     private void addSeries(Readings readings, String label) {
-        readingsChart.getData().addAll(newSeries(readings, label));
+        XYChart.Series series = newSeries(readings, label);
+        readingsChart.getData().addAll(series);
+        seriesMap.put(label, series);
     }
 
     private void setSeries(Readings readings, String label) {
-        readingsChart.getData().setAll(newSeries(readings, label));
+        XYChart.Series series = newSeries(readings, label);
+        readingsChart.getData().setAll(series);
+        seriesMap.remove("ALL");
+        seriesMap.put(label, series);
     }
-
 
     private void populateChart() {
         String[] wapsArray = new String[metaData.getNumberOfMacs()];
@@ -92,11 +126,6 @@ public class Controller implements Initializable, ChangeListener<Number> {
         waps.setCategories(wapNames);
         waps.setTickLabelRotation(90);
         setSeries(readings, "ALL");
-    }
-
-    private void populateRoomCombo() {
-        for(Room room: Room.values())
-            roomCombo.getItems().add(room.toString());
     }
 
     private void escuchador(Number a, Number b, Number c) {
